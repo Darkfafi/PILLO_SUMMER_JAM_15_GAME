e9d96cb66deed64b005e083b2cd5cea84e4ddf83
Nickabilla <nick.aarts01@gmail.com>
2015-08-23 11:02:12 +0200

    Added the lovely breathe script

---
+++ Assets/_Scripts/BreatheScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BreatheScript : MonoBehaviour {
+	float timer;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		timer += Time.deltaTime;
+		
+        Vector3 newScale = transform.localScale;
+		newScale.x += Mathf.Sin(timer * 4) * 0.01f;
+        newScale.y += Mathf.Cos(timer * 4) * 0.01f;
+		transform.localScale = newScale;
+
+        Vector3 newRotation = transform.eulerAngles;
+		newRotation.z += Mathf.Sin(timer * 4.5f + 180) * 0.15f;
+        transform.eulerAngles = newRotation;
+
+    }
+}
