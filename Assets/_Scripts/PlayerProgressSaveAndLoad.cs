ceebc50c665ce3336a32354808a6e78051f53732
Nickabilla <nick.aarts01@gmail.com>
2015-08-23 11:54:05 +0200

    added save and load functionality

---
+++ Assets/_Scripts/PlayerProgressSaveAndLoad.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerProgressSaveAndLoad : MonoBehaviour {
+	
+	public static void SetLevel(int l)
+	{
+        PlayerPrefs.SetInt("level", l);
+    }
+	
+	public static int GetLevel()
+	{
+		if (!PlayerPrefs.HasKey("level"))
+        {
+			PlayerPrefs.SetInt("level", 1);
+		}
+        return PlayerPrefs.GetInt("level");
+    }
+}
