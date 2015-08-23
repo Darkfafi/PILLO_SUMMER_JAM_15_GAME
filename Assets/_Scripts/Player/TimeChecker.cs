f4c27524760986bd860b2bcb70e1e4fd2ef79392
Patrick-Aubroeck <P.aubroeck@hotmail.com>
2015-08-23 13:13:47 +0200

    timechecker fix + characterselect.scene

--- Assets/_Scripts/Player/TimeChecker.cs
+++ Assets/_Scripts/Player/TimeChecker.cs
@@ -9,26 +9,29 @@ public class TimeChecker : MonoBehaviour {
 	private float sensitivityPillowTwo;
 	// Use this for initialization
 	void Start () {
-
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		sensitivityPillowOne = PilloController.GetSensor (Pillo.PilloID.Pillo1);
 		sensitivityPillowTwo = PilloController.GetSensor (Pillo.PilloID.Pillo2);
-		if (sensitivityPillowOne == 1.0f) {
+		if (sensitivityPillowOne >= 0.95f) {
 			timerPillowOne += Time.deltaTime;
+			print(timerPillowOne + "pilloOne");
 		}
-		if (sensitivityPillowTwo == 1.0f) {
+		if (sensitivityPillowTwo >= 0.95f) {
 			timerPillowTwo += Time.deltaTime;
+			print(timerPillowTwo + "pilloTwo");
 		}
 		if (timerPillowOne >= 3.0f && timerPillowTwo <= 3.0f)
 		{
 			//playertextures 1
+			print ("1");
 		}
 		if (timerPillowTwo >= 3.0f && timerPillowOne <= 3.0f) 
 		{
 			//playertextures 2
+			print ("2");
 		}
 		if (timerPillowOne >= 3.0f && timerPillowTwo >= 3.0f)
 		{
@@ -36,9 +39,11 @@ public class TimeChecker : MonoBehaviour {
 			float random = Random.Range(1,10);
 			if(random > 5)
 			{
+				print ("1.1");
 				//playerOne
 			}else{
 				//playerTwo
+				print ("2.1");
 			}
 		}
 	}
